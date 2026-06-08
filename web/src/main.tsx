a46c71c8ada2e4589532d5426dc8c6799a998e99
hdk-21 <hokdung@outlook.com>
2026-06-08 13:07:34 +0800

    fix(web): disable visual focus rings on mouse click using FocusStyleManager

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -2,9 +2,11 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import "./i18n/config";
 import App from "./App.tsx";
-import { OverlaysProvider } from "@blueprintjs/core";
+import { OverlaysProvider, FocusStyleManager } from "@blueprintjs/core";
 import { BrowserRouter } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
+
+FocusStyleManager.onlyShowFocusOnTabs();
 
 createRoot(document.getElementById("root")!).render(
   <OverlaysProvider>
